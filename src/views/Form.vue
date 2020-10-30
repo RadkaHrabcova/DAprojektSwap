e6e2054347573e3251c52b0ace0c1977e50513f8
RadkaHrabcova <radkahrabcova@gmail.com>
2020-10-30 19:02:12 +0100

    úprava formuláře

--- src/views/Form.vue
+++ src/views/Form.vue
@@ -1,38 +1,71 @@
 <template>
-  <div>
-    <h1>Formulář</h1>
-        <ul>
-            <li>
-                <p> Jméno</p>
-                <input type="text" v-model="jmeno" >
-            </li>
-            <li>
-                <p> Příjmení</p>
-                <input type="text" v-model="prijmeni">
-            </li>
-            <li>
-                <p> Název zboží</p>
-                <input type="text" v-model="zbozi">
-            </li>
-            <li>
-                <p> Kategorie</p>
+  <form class="inzerat" method="POST" action="">
+    <h1>Detail inzerátu</h1>
+        
+            
+                <h2>Vyberte foto </h2>
+                <button>Vybrat soubor</button>
+           
+                <h2> Vyberte typ</h2>
+                <input type="radio" name="typInzeratu" value="nabizim" v-model="typ">Nabízím
+                <input type="radio" name="typInzeratu" value="poptavam" v-model="typ">Poptávám
+           
+                <h2> Vyberte Kategorii</h2>
                 <select v-model="kategorie">
-                    <option value="kvetiny">kvetiny</option>
-                    <option value="hracky">hracky</option>
-                    <option value="obleceni">obleceni</option>
-                    <option value="domacnost">domacnost</option>
-                    <option value="jine">jine</option>
-                <select/>
-            </li>
-        </ul>
-  </div>
+                    <option value="kvetiny">Květiny</option>
+                    <option value="hracky">Hračky</option>
+                    <option value="obleceni">Oblečení</option>
+                    <option value="domacnost">Domácnost</option>
+                    <option value="jine">Jiné</option>
+                </select>
+          
+                <h2> Název produktu</h2>
+                <input type="text" v-model="nazev">
+          
+                <h2> Popis </h2>
+                <textarea v-model="popis"></textarea>
+           
+                <h2> Ulice</h2>
+                <input type="text" v-model="ulice">
+            
+                <h2> Město</h2>
+                <input type="text" v-model="mesto">
+            
+                <h2> PSČ</h2>
+                <input type="text" v-model="psc">
+            
+                <h2> Vyměním za...</h2>
+                <textarea v-model="vymenim"></textarea>
+
+                <button>Přidat inzerát</button>
+            
+  </form>
 </template>
 
 <script>
+export default{
+   data(){
+       return{
+        typ:"nabizim",
+        kategorie:"domacnost",
+        nazev:"Dřevěná skříň...",
+        popis:"Zde popište zboží",  
+        ulice:"",
+        mesto:"",
+        psc:"",
+        vymenim:"Zde napište za co, chcete zboží vyměnit",
 
+        
+       };
+   },
+       
+    
+
+}
 
   
 </script>
 
 <style>
+
 </style>
