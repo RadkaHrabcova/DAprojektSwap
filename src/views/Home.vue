a4ed07f0055b4cf14d413c7f4ee0c962b6f45e13
RadkaHrabcova <radkahrabcova@gmail.com>
2020-11-21 09:25:53 +0100

    Oprava velkého písmena

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="home">
       <p>home swapito</p>
-      <router-link to="/form">Přidej inzerát</router-link>
+      <router-link to="/form">přidej inzerát</router-link>
       <br/>
       <br/>
       <button v-if="userIsSignedIn" @click="signOut">Odhlásit se</button>
