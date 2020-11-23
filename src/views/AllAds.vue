6c233df6d2916051b54e037c5c95d1392c4faa59
RadkaHrabcova <radkahrabcova@gmail.com>
2020-11-23 21:14:37 +0100

    change of endponts

--- src/views/AllAds.vue
+++ src/views/AllAds.vue
@@ -1,18 +1,10 @@
 <template>
   <div class="AllAds">
-    
     <h2>Výpis inzerátů</h2>
 
-      <v-row justify="center">
-        <v-col
-          v-for="ad in ads" :key="ad.id"
-          cols="12"
-          sm="8"
-        >
-          <v-card
-          class="mx-auto ad-border"
-          max-width="344"
-        >
+    <v-row justify="center">
+      <v-col v-for="ad in ads" :key="ad.id" cols="12" sm="8">
+        <v-card class="mx-auto ad-border" max-width="344">
           <v-img
             :src="ad.picture"
             max-height="180px"
@@ -20,38 +12,28 @@
             class="img-margin"
           ></v-img>
 
-          <v-card-title
-          >
-            {{ad.name}}
+          <v-card-title>
+            {{ ad.name }}
           </v-card-title>
 
           <v-card-subtitle>
-            {{ad.type}}
+            {{ ad.type }}
           </v-card-subtitle>
 
-            <v-btn
-              color="green darken-2"
-              text
-              @click="show = !show"
-            >
-              Detail inzerátu
-            </v-btn>
+          <v-btn color="green darken-2" text @click="show = !show">
+            Detail inzerátu
+          </v-btn>
 
-              <v-card-text
-              v-show="show"
-              >
-                {{ad.description}}
-              </v-card-text>
-
+          <v-card-text v-show="show">
+            {{ ad.description }}
+          </v-card-text>
         </v-card>
       </v-col>
-      </v-row>
-
+    </v-row>
   </div>
 </template>
 
 <script>
-
 export default {
   data() {
     return {
@@ -61,7 +43,7 @@ export default {
   },
   methods: {
     fetchAds() {
-      fetch("http://127.0.0.1:5000/ads")
+      fetch("https://beta-swapito-main-sv1kp3pz6lex.herokuapp.com/ads")
         .then((response) => {
           if (response.ok) {
             return response.json();
@@ -85,13 +67,12 @@ export default {
 </script>
 
 <style scoped>
-.ad-border{
+.ad-border {
   border: 1px solid lightgrey;
   border-radius: 10px;
 }
 
-.img-margin{
+.img-margin {
   margin-top: 10px;
-}
-</style>>
-
+}</style
+>>
